fd92b5b0817ab28efee81c9f96edd80c71f43202
Christopher Serr <christopher.serr@gmail.com>
2018-05-01 17:28:01 +0200

    Remove Rom Hack Compiler

--- compiler/src/opt.rs
+++
@@ -1,9 +0,0 @@
-#[derive(StructOpt, Debug)]
-pub enum Opt {
-    /// Builds the Rom Hack
-    #[structopt(name = "build")]
-    Build {},
-    /// Creates a new Rom Hack with the given name
-    #[structopt(name = "new")]
-    New { name: String },
-}
